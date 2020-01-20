f5677d39f551d00ac8f7aaeeb6208d69fac5736b
Dinesh <dineshthiru.1998@gmail.com>
2020-01-20 10:43:43 +0530

    changes apidoc to docs

--- backend/routes/newsapi.js
+++ backend/routes/newsapi.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const { getNews } = require('../controllers/newsapi');
-<<<<<<< HEAD
 /**
  * @api {get} /country/:countryId/category/:categoryId/page/:pageno Request News Information from NewsApi
  * @apiName getNews
@@ -13,9 +12,6 @@ const { getNews } = require('../controllers/newsapi');
  * @apiParam {Number} PageNo For Pagination Purpose
  * @apiSuccess {Array} news Array of objects of news article
  */
-=======
-
->>>>>>> 5cf1018274f328947e1106f5c258345455262689
 router
     .route('/country/:countryId/category/:categoryId/page/:pageno')
     .get(getNews);
