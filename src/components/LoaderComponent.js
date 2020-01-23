2fb7fd5a9a3c70e3bf16cf7620a1adcc90d30f74
Dinesh <dineshthiru.1998@gmail.com>
2020-01-23 11:28:24 +0530

    implemented morgan and winston logger

---
+++ src/components/LoaderComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
+
+const LoaderComponent = () => (
+  <Segment>
+    <Dimmer active>
+      <Loader />
+    </Dimmer>
+
+    <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+  </Segment>
+)
+
+export default LoaderComponent
