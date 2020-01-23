bab4577933357c6ed959ac655ecf6e95965cd25f
Dinesh <dineshthiru.1998@gmail.com>
2020-01-23 17:16:12 +0530

    msgpack encoding to save logs in Redis

--- backend/logs/logtransporter/redislogsave.js
+++ backend/logs/logtransporter/redislogsave.js
@@ -1,5 +1,25 @@
 const {client} = require('../../config/redisconfig');
+const msgpack = require("msgpack-lite");
+const sizeof = require('object-sizeof');
+ 
 
 exports.RedisLogSave = async (message) => {
-    await client.LPUSH('logs', message);
+
+    console.log(sizeof(message))
+
+    // encode from JS Object to MessagePack (Buffer)
+    const buffer = msgpack.encode(message);
+    //console.log(Buffer.byteLength(buffer));
+    console.log(sizeof(buffer));
+    
+
+    await client.LPUSH('logs', buffer);
+    await client.LRANGE('logs', 0, -1, async (err, reply) => {
+        if (err) console.log(err); 
+        console.log(reply)
+        await reply.forEach(element => {
+            console.log(msgpack.decode(msgpack.encode(element)))
+        });
+    });
+
 };
