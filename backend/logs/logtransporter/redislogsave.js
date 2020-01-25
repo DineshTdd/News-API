8194d172f890c3a7b44571274aca4bf545e78e55
Dinesh <dineshthiru.1998@gmail.com>
2020-01-25 09:55:28 +0530

    JSON to Buffer implemented

--- backend/logs/logtransporter/redislogsave.js
+++ backend/logs/logtransporter/redislogsave.js
@@ -1,25 +1,47 @@
 const {client} = require('../../config/redisconfig');
-const msgpack = require("msgpack-lite");
+// const msgpack = require("msgpack-lite");
 const sizeof = require('object-sizeof');
+const { performance, PerformanceObserver } = require('perf_hooks');
+
+
  
 
 exports.RedisLogSave = async (message) => {
-
+    try{
+    
     console.log(sizeof(message))
 
     // encode from JS Object to MessagePack (Buffer)
-    const buffer = msgpack.encode(message);
-    //console.log(Buffer.byteLength(buffer));
-    console.log(sizeof(buffer));
+    // let buffer = msgpack.encode(JSON.stringify(message));
+
+    // Observes high resolution performance metrics
+    const obs = new PerformanceObserver((items) => {
+        console.log('It took ' + items.getEntries()[0].duration + ' ms.');
+        performance.clearMarks();
+      });
+    obs.observe({ entryTypes: ['measure'] });
+
+
+    performance.mark('A')
+    const buffer = Buffer.from(JSON.stringify(message), 'ascii'); 
+    performance.mark('B')
+    performance.measure('A to B', 'A', 'B') // It took 0.174399 ms.
     
+    console.log(sizeof(buffer));
 
     await client.LPUSH('logs', buffer);
     await client.LRANGE('logs', 0, -1, async (err, reply) => {
         if (err) console.log(err); 
-        console.log(reply)
+        //console.log(reply)
         await reply.forEach(element => {
-            console.log(msgpack.decode(msgpack.encode(element)))
+            //console.log(msgpack.decode(msgpack.encode(element)))
+            // let result = msgpack.decode(msgpack.encode(element)).replace(/[\u{0080}-\u{FFFF}|]/gu,"")
+            // element = element.replace(/\\"/, "'");
+            // result = result.replace(/{.}/g, '');
+            console.log(JSON.parse(element).message)
         });
     });
-
+    } catch(err) {
+        console.log(err)
+    }
 };
