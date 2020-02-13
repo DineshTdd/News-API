35022c7dbabe52ed55d2d214f8375ca3acfded90
Dinesh <dineshthiru.1998@gmail.com>
2020-02-13 11:08:36 +0530

    restructured 'UserActivity' to bring responsiveness into action!

--- src/components/Chart/Barchart/Chart.js
+++ src/components/Chart/Barchart/Chart.js
@@ -1,49 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { scaleBand, scaleLinear } from 'd3-scale';
 import { timeParse } from 'd3-time-format';
-import ResponsiveWrapper from './ResponsiveWrapper'
+
 import Axes from './Axes';
 import Bars from './Bars';
-import {connect} from 'react-redux';
+import { useSelector } from 'react-redux';
 
-
-class Chart extends Component {
-  constructor() {
-    super()
-    this.xScale = scaleBand()
-    this.yScale = scaleLinear()
-  }
-
-  render() {
-    const parseDate = timeParse("%Y-%m-%d");
-    let {graphData} = this.props;
-    let barData = [];
-    graphData = graphData.filter((datum, index) => index<7)
-    graphData.map((datum) => barData.push({x: parseDate(datum.date), y: datum.totalDuration}))
-    barData.sort((a, b) => new Date(a.x) - new Date(b.x))
-    const margins = { top: 50, right: 20, bottom: 100, left: 60 };
-    const svgDimensions = { 
-        width: Math.max(this.props.parentWidth, 300), 
-        height: 275 
-    };
-
-    const maxValue = Math.max(...barData.map(d => d.y))
-    
-    // scaleBand type
-    const xScale = this.xScale
-      .padding(0.5)
-      // scaleBand domain should be an array of specific values
-      // in our case, we want to use movie titles
-      .domain(barData.map(d => d.x))
-      .range([margins.left, svgDimensions.width - margins.right])
+const Chart = (props) => {
   
-     // scaleLinear type
-    const yScale = this.yScale
-       // scaleLinear domain required at least two values, min and max       
-      .domain([0, maxValue])
-      .range([svgDimensions.height - margins.bottom, margins.top])
+
+  const parseDate = timeParse("%Y-%m-%d");
+  let graphData = useSelector(state => state.logs.graph);
+  let barData = [];
+  graphData = graphData.filter((datum, index) => index<7)
+  graphData.map((datum) => barData.push({x: parseDate(datum.date), y: datum.totalDuration}))
+  barData.sort((a, b) => new Date(a.x) - new Date(b.x))
+  const margins = { top: 50, right: 20, bottom: 100, left: 60 };
+  const svgDimensions = { 
+      width: props.containerWidth, 
+      height: props.containerHeight
+  };
+
+  const maxValue = Math.max(...barData.map(d => d.y))
+  
+  // scaleBand type
+  const xScale = scaleBand()
+    .padding(0.5)
+    // scaleBand domain should be an array of specific values
+    // in our case, we want to use movie titles
+    .domain(barData.map(d => d.x))
+    .range([margins.left, svgDimensions.width - margins.right])
+
+    // scaleLinear type
+  const yScale = scaleLinear()
+      // scaleLinear domain required at least two values, min and max       
+    .domain([0, maxValue])
+    .range([svgDimensions.height - margins.bottom, margins.top])
+
+    
 
     return (
+      <div>
       <svg width={svgDimensions.width} height={svgDimensions.height}>
          <Axes 
             scales={{ xScale, yScale }}
@@ -58,15 +55,19 @@ class Chart extends Component {
           svgDimensions={svgDimensions}
         />
       </svg>
+      </div>
     )
   }
-}
 
-const mapStateToProps = state => {
-    return {
-        graphData: state.logs.graph,
-    };
-};
+// const mapStateToProps = state => {
+//     return {
+//         graphData: state.logs.graph,
+//     };
+// };
 
 
-export default ResponsiveWrapper(connect(mapStateToProps,null)(Chart));
+// export default ResponsiveWrapperHook(
+  // connect(mapStateToProps,null)
+  // (Chart));
+
+  export default Chart;
--- src/components/UserActivity.js
+++ src/components/UserActivity.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import * as logsAction from '../store/action/logsAction';
+// import { useResponsiveWrapper } from './Chart/Barchart/ResponsiveWrapperHook';
 // import UsageActivityChart from '../components/UsageActivityChart';
 import Chart from './Chart/Barchart/Chart';
 import Pie from './Chart/Piechart/Pie';
@@ -8,125 +9,125 @@ import { timeParse } from 'd3-time-format';
 import './Styles/NumericStyle.css'
 import {Message, Feed, Dimmer, Loader, Icon, Segment } from 'semantic-ui-react';
 
-class UserActivity extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isActivityLoading: false,
+const UserActivity = (props) => {
+    const dispatch = useDispatch()
+
+    const [isActivityLoading, setIsActivityLoading] = useState(false);
+    
+    useEffect(() => {
+        setIsActivityLoading(true)
+        const fetchData = () => {
+            dispatch(logsAction.fetchUserActivityLogs())
         }
-    }
+        fetchData()
+        setTimeout(() => setIsActivityLoading(false), 1000)
+    }, [dispatch])
 
-    async componentDidMount() {
-        this.setState({isActivityLoading: true})
-        await this.props.fetchUserActivityLogs();
-        this.setState({isActivityLoading: false})
-        
-    }
+    let {userActivityLogs: logsData, graph: graphData } = useSelector(state => state.logs);
+    let profileImage = useSelector(state => state.user.userData.userProfilePicture)
+    let averageTime = 0;
+    graphData = graphData.filter((datum,index) => index<7)
+    graphData.map(item => {averageTime += item.totalDuration; return item})
+    averageTime = Math.ceil(averageTime / graphData.length)
+    let barData = [];
+    const parseDate = timeParse("%Y-%m-%d");
+    graphData.map((datum) => barData.push({x: parseDate(datum.date), y: datum.totalDuration}))
+    barData.sort((a, b) => new Date(a.x) - new Date(b.x))
 
-    render() {
-        const {logsData} = this.props;
-        let { graphData } = this.props;
-        let averageTime = 0;
-        graphData = graphData.filter((datum,index) => index<7)
-        graphData.map(item => {averageTime += item.totalDuration; return item})
-        averageTime = Math.ceil(averageTime / graphData.length)
-        let barData = [];
-        const parseDate = timeParse("%Y-%m-%d");
-        graphData.map((datum) => barData.push({x: parseDate(datum.date), y: datum.totalDuration}))
-        barData.sort((a, b) => new Date(a.x) - new Date(b.x))
-        return (this.state.isActivityLoading) 
-                ? (
-                    <Dimmer active>
-                    <Loader size='small' /><br></br>
-                    <h5>Loading activity...</h5>
-                    </Dimmer>
-                )
-                : (<div>
-                { (logsData.length === 0) ? 
-                    (
-                        <Message style={{ width: '50%', marginLeft: '25%'}} info>
-                            <Message.Header>No activities right now!</Message.Header>
-                            <p>Explore the app</p>
-                        </Message>
-                    )
-                    : (
-                        <div>
-                        <Segment>
-                            <h3>Time on NewsApp</h3>
-                            <div style={{ textAlign: 'center' }}>
-                                <p className="averageTime">{averageTime}m</p>
-                                <p style={{fontWeight: "bold"}}>Daily Average</p>
-                                <p>Average time you spent per day using the news app in the last week</p>
-                            </div>
-                            <div >
-                            {/* <UsageActivityChart width={null} height={null} /> */}
-                            <Chart />
-                            <div style={{marginLeft: '30%'}}>
-                            <Pie
-                                data={barData}
-                                width={200}
-                                height={200}
-                                innerRadius={60}
-                                outerRadius={100}
-                            /> </div>
-                            </div>
-                        </Segment>
-                        <Feed>
-                        {logsData.map((item) => (
-                                <Feed.Event key={item._id}>
-                                    <Feed.Label>
-                                        <div style={{
-                                            display: 'inline-block',
-                                            position: 'relative',
-                                            width: '40px',
-                                            height: '40px',
-                                            overflow: 'hidden',
-                                            borderRadius: '50%'
-                                        
-                                        }}>
-                                        <img 
-                                        src={
-                                            (this.props.profileImage !== null) 
-                                            ? this.props.profileImage 
-                                            : 'https://react.semantic-ui.com/images/avatar/large/matthew.png' 
-                                        } 
-                                        alt="userProfilePic"
-                                        style={{
-                                            width: 'auto',
-                                            height: '100%',
-                                            marginLeft: '-2.5px',
-                                        }}/>
-                                        </div>
-                                    </Feed.Label>
-                                    <Feed.Content>
-                                        <Feed.Summary>
-                                            <Feed.User>Usage Activity: </Feed.User> <Icon name='history' size='small' /> {item.content} {item.usage.entryTime}
-                                            <Feed.Date>used for {item.usage.activeMinutes}</Feed.Date>
-                                        </Feed.Summary>
-                                    </Feed.Content>
-                                </Feed.Event>
-                        ))}
-                        </Feed>
-                        </div>
-                    )
-                }
-                </div>
+    
+    return (isActivityLoading)
+            ? (
+                <Dimmer active>
+                <Loader size='small' /><br></br>
+                <h5>Loading activity...</h5>
+                </Dimmer>
             )
-    }
+            : (<div>
+            { (logsData.length === 0) ? 
+                (
+                    <Message style={{ width: '50%', marginLeft: '25%'}} info>
+                        <Message.Header>No activities right now!</Message.Header>
+                        <p>Explore the app</p>
+                    </Message>
+                )
+                : (
+                    
+                    <ChartRenderer {...{averageTime, barData, logsData, profileImage}} />
+                )
+            }
+            </div>
+        )
+
 }
-const mapStateToProps = state => {
-    return {
-        logsData: state.logs.userActivityLogs,
-        graphData: state.logs.graph,
-        profileImage: state.user.userData.userProfilePicture
-    };
-};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        fetchUserActivityLogs: () => dispatch(logsAction.fetchUserActivityLogs()),
-    };
-};
+export default UserActivity;
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserActivity);
+function ChartRenderer({averageTime, barData, logsData, profileImage}) {
+    const Elem = useRef();
+    const [ containerWidth, setContainerWidth ] = useState(500);
+
+    useEffect(() => {
+        if(Elem !== null) {
+        setContainerWidth(Elem.current.offsetWidth)
+        }
+    }, []);
+
+    useEffect(() => { 
+        let fitParentContainer;
+        if(Elem !== null) {
+        fitParentContainer = () => {
+            setContainerWidth(Elem.current.offsetWidth)
+        }
+        }
+        window.addEventListener('resize', fitParentContainer)
+        return () => { window.removeEventListener('resize', fitParentContainer) }
+    }, [])
+
+    return <div>
+        <Segment>
+            <h3>Time on NewsApp</h3>
+            <div style={{ textAlign: 'center' }}>
+                <p className="averageTime">{averageTime}m</p>
+                <p style={{ fontWeight: "bold" }}>Daily Average</p>
+                <p>Average time you spent per day using the news app in the last week</p>
+            </div>
+            <div ref={Elem} style={{ width: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <div >
+                <Chart containerHeight={300} containerWidth={containerWidth} />
+                </div>
+                <div>
+                    <Pie data={barData} width={200} height={200} innerRadius={60} outerRadius={100} />
+                </div>
+            </div>
+        </Segment>
+        <Feed>
+            {logsData.map((item) => (<Feed.Event key={item._id}>
+                <Feed.Label>
+                    <div style={{
+                        display: 'inline-block',
+                        position: 'relative',
+                        width: '40px',
+                        height: '40px',
+                        overflow: 'hidden',
+                        borderRadius: '50%'
+                    }}>
+                        <img src={(profileImage !== null)
+                            ? profileImage
+                            : 'https://react.semantic-ui.com/images/avatar/large/matthew.png'} alt="userProfilePic" style={{
+                                width: 'auto',
+                                height: '100%',
+                                marginLeft: '-2.5px',
+                            }} />
+                    </div>
+                </Feed.Label>
+                <Feed.Content>
+                    <Feed.Summary>
+                        <Feed.User>Usage Activity: </Feed.User> <Icon name='history' size='small' /> {item.content} {item.usage.entryTime}
+                        <Feed.Date>used for {item.usage.activeMinutes}</Feed.Date>
+                    </Feed.Summary>
+                </Feed.Content>
+            </Feed.Event>))}
+        </Feed>
+    </div>;
+}
 
